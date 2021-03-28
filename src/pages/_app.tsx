da89ebc45d436ed29bb411655d0776d5d4038f7a
shuta13 <hollystarsun@gmail.com>
2021-03-29 03:13:03 +0900

    update: add header, footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,35 @@ import '../assets/styles/global.css';
 import '../assets/styles/markdown.css';
 import type { AppProps } from 'next/dist/next-server/lib/router/router';
 import 'prismjs/themes/prism-tomorrow.css';
+import Link from 'next/link';
+import { BlogTitle } from '../config';
+
+const Header: React.FC = () => (
+  <header>
+    <h1>
+      <Link href="/">
+        <a>{BlogTitle}</a>
+      </Link>
+    </h1>
+  </header>
+);
+
+const Footer: React.FC = () => (
+  <footer>
+    <Link href="https://did0es.me" passHref={true}>
+      <a>© 2021 did0es</a>
+    </Link>
+  </footer>
+);
 
 const BlogApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 };
 
 export default BlogApp;
