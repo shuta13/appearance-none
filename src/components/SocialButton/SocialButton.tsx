e4cdf152e2f56b41c80f07ef6557b274b05c79b0
shuta13 <hollystarsun@gmail.com>
2021-03-31 09:18:32 +0900

    fix

--- src/components/SocialButton/SocialButton.tsx
+++ src/components/SocialButton/SocialButton.tsx
@@ -11,7 +11,7 @@ export const SocialButton: React.FC<Props> = (props) => {
   const { href, icon } = props;
   return (
     <Link href={href}>
-      <a>
+      <a rel="nofollow noreferrer noopener" target="_blank">
         <FontAwesomeIcon icon={icon} className={styles.icon} size="2x" />
       </a>
     </Link>
